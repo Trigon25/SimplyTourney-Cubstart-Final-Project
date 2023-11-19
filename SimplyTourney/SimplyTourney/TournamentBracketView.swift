ba521617a71890f1d72a45159355936443c63e81
Daniel Taveras <me@danieltaveras.com>
2023-11-19 14:17:02 -0800

    add initial tournament bracket view

--- SimplyTourney/SimplyTourney/TournamentBracketView.swift
+++ SimplyTourney/SimplyTourney/TournamentBracketView.swift
@@ -8,11 +8,157 @@
 import SwiftUI
 
 struct TournamentBracketView: View {
+  let bracket: TournamentBracket = getTestTournamentBracket();
+
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      Text(bracket.name)
+      ScrollView(.horizontal) {
+        LazyHStack {
+          ForEach(bracket.rounds) {  round in
+            VStack {
+              Text(round.name)
+              ForEach(round.matches) {
+                match in
+                MatchView(match: match)
+              }
+            }
+          }
+        }
+        .scrollTargetLayout()
+      }
+      .scrollTargetBehavior(.viewAligned)
+      .safeAreaPadding(.horizontal, 40)
     }
 }
 
+struct MatchView: View {
+  let match: TournamentMatch
+  var isFirstPlayerWinner: Bool {
+    match.winner == match.firstPlayer
+  }
+  var isSecondPlayerWinner: Bool {
+    match.winner == match.secondPlayer
+  }
+  var body: some View {
+    VStack {
+      Text("\(match.firstPlayer.name): \(match.firstPlayerScore)\(isFirstPlayerWinner ? " 🏆":"")")
+        .opacity(isFirstPlayerWinner ? 1.0 : 0.6)
+        .fontWeight(isFirstPlayerWinner ? .bold : .regular)
+      Text("\(match.secondPlayer.name): \(match.secondPlayerScore)\(isSecondPlayerWinner ? " 🏆":"")")
+        .opacity(isSecondPlayerWinner ? 1.0 : 0.6)
+        .fontWeight(isSecondPlayerWinner ? .bold : .regular)
+    }
+    .frame(width: 250.0)
+    .padding()
+    .background(.cyan)
+  }
+}
+
+struct TournamentBracket: Identifiable {
+  let name: String
+  let players: [TournamentPlayer]
+  let rounds: [TournamentRound]
+  var id: String {
+      name
+  }
+}
+
+struct TournamentRound: Identifiable {
+  let name: String
+  let matches: [TournamentMatch]
+  var id: String {
+    name
+  }
+}
+
+struct TournamentMatch: Identifiable {
+    let firstPlayer: TournamentPlayer
+    let secondPlayer: TournamentPlayer
+    let firstPlayerScore: Int
+    let secondPlayerScore: Int
+
+    var id: String {
+      firstPlayer.name + secondPlayer.name
+    }
+
+  var winner: TournamentPlayer {
+    if (firstPlayerScore == secondPlayerScore) {
+      // Need to handle draw condition
+      return TournamentPlayer(name: "Draw")
+    }
+
+    return firstPlayerScore > secondPlayerScore
+    ? firstPlayer : secondPlayer
+  }
+}
+
+struct TournamentPlayer: Identifiable, Equatable {
+  let name: String
+  var id: String {
+    name
+  }
+}
+
+func getTestMatches(players: [TournamentPlayer]) -> [TournamentMatch] {
+  var playerPool = players.shuffled();
+  var result: [TournamentMatch] = []
+  while !playerPool.isEmpty {
+    let firstPlayer = playerPool.removeFirst()
+    let secondPlayer = playerPool.removeFirst()
+    let firstPlayerScore = Int.random(in: 0...100)
+    let secondPlayerScore = Int.random(in: 0...100)
+    let match = TournamentMatch(
+      firstPlayer: firstPlayer,
+      secondPlayer: secondPlayer,
+      firstPlayerScore: firstPlayerScore,
+      secondPlayerScore: secondPlayerScore
+    )
+    result.append(match)
+  }
+  return result
+}
+
+func getTestTournamentBracket() -> TournamentBracket {
+
+  let players = [
+    TournamentPlayer(name: "player one"),
+    TournamentPlayer(name: "player two"),
+    TournamentPlayer(name: "player three"),
+    TournamentPlayer(name: "player four"),
+    TournamentPlayer(name: "player five"),
+    TournamentPlayer(name: "player six"),
+    TournamentPlayer(name: "player seven"),
+    TournamentPlayer(name: "player eight"),
+    TournamentPlayer(name: "player nine"),
+    TournamentPlayer(name: "player ten"),
+    TournamentPlayer(name: "player eleven"),
+    TournamentPlayer(name: "player twelve"),
+    TournamentPlayer(name: "player thirteen"),
+    TournamentPlayer(name: "player fourteen"),
+    TournamentPlayer(name: "player fifteen"),
+    TournamentPlayer(name: "player sixteen")
+  ];
+
+  let eigthsMatches = getTestMatches(players: players);
+  let quarterFinalsMatches = getTestMatches(players: eigthsMatches.map({ $0.winner }))
+  let semiFinalsMatches = getTestMatches(players: quarterFinalsMatches.map({ $0.winner }))
+  let finalsMatches = getTestMatches(players: semiFinalsMatches.map({ $0.winner }))
+
+
+  let eigths = TournamentRound(name: "Eighth-finals", matches: eigthsMatches)
+  let quarterFinals = TournamentRound(name: "Quarterfinals", matches: quarterFinalsMatches)
+  let semiFinals = TournamentRound(name: "Semifinals", matches: semiFinalsMatches)
+  let finals = TournamentRound(name: "Finals", matches: finalsMatches)
+  let rounds = [eigths, quarterFinals, semiFinals, finals]
+
+  return TournamentBracket(
+    name: "My Tournament",
+    players:  players,
+    rounds: rounds
+  )
+}
+
 #Preview {
-    TournamentBracketView()
+  TournamentBracketView()
 }
