c69dbe8fa7a1ce8a7c760005b5fb6c6b3e103686
Jack Tian <jimball234@berkeley.edu>
2023-11-22 19:39:48 -0800

    update StartingView

--- SimplyTourney/SimplyTourney/StartingView.swift
+++ SimplyTourney/SimplyTourney/StartingView.swift
@@ -10,14 +10,46 @@ import SwiftUI
 struct StartingView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            NavigationStack {
+                VStack {
+                    Text("Simply**Tourney**").font(.largeTitle).padding(.top, 50)
+                    Spacer()
+                    NavigationLink {
+                        CreateNewTourneyBracketView()
+                    } label: {
+                        Text("**Create New Tourney**").font(.title)
+                    }
+                    //title for created tournaments
+                    
+                    //links to existing tournaments
+                    
+                    //button to make new one
+                    .foregroundColor(.white)
+                    .frame(width: 320, height: 50)
+                    .background(RoundedRectangle(cornerRadius:25))
+                    .padding(.bottom, 100)
+                    NavigationLink {
+                        TournamentBracketView()
+                    } label: {
+                        Text("**Frogger Tourney**").font(.title)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 320, height: 50)
+                    .background(RoundedRectangle(cornerRadius:25))
+                    .padding(.bottom, 300)
+                    
+                }.navigationDestination(for: String.self) { value in
+                }
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                .background(LinearGradient(gradient: Gradient(colors: [.blue, Color(red:0.4627, green:0.8392, blue:1.0)]), startPoint: .top, endPoint: .bottom))
+            }
+            
             
         }
-        .padding()
+        
+                
     }
+        
 }
 
 #Preview {
